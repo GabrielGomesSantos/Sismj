171810e51a5d069ba6cde0d35fa4022a023da8af
Gabriel Gomes <gabrielgones5@gmail.com>
2024-07-10 20:18:47 -0300

    .

--- src/pages/navbar.php
+++ src/pages/navbar.php
@@ -1,18 +1,25 @@
 <nav class="navbar navbar-expand-lg navbar-dark"  style="background-color: #17a2b8;">
     <div class="container-fluid ">
 
-      <img src="../../assets/images/logo.png" alt="" width="40px">
+      <!-- Logo e Nome  -->
 
-      <a class="navbar-brand" href="">Sisman</a> 
+      <a class="navbar-brand" href="#">
+        <img src="../../assets/images/logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+        Sisman
+      </a>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <!-- Logo e Nome  -->
 
-          <!-- <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li> -->
+      <!-- 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
-        </ul>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+
+          </ul>
+      -->
       </div>
     </div>
   </nav>
