908ae85cbdc9c2ec518346b1cd7301d73a6ecb4c
Gabriel Gomes <gabrielgones5@gmail.com>
2024-07-10 21:06:24 -0300

    Footer

--- src/pages/cadastrar_funcionario.php
+++ src/pages/cadastrar_funcionario.php
@@ -1,20 +1,28 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
-
     <link rel="stylesheet" href="fonts/icomoon/style.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <!-- Required meta tags -->
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
+    <!-- Bootstrap CSS -->
+
     <!-- Style -->
     <link rel="stylesheet" href="../../assets/css/style.css">
+    <!-- Style -->
 
+    <!-- Fonte Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Fonte Awesome -->
+    
+    <!-- Titulo Lembrar de mudar! -->
     <title>Cadastro de Funcionario</title>
 </head>
 
@@ -82,6 +90,8 @@
     <script src="../../assets/js/popper.min.js"></script>
     <script src="../../assets/js/bootstrap.min.js"></script>
     <script src="../../assets/js/main.js"></script>
+    <?php include('footer.php')?>
 </body>
+
 
 </html>
