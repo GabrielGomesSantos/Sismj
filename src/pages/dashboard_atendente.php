f965f08e31387a0162bed4aad84c1064bd8d3a5f
Gabriel Gomes <gabrielgones5@gmail.com>
2024-08-07 16:10:09 -0300

    insercao da banco de dados

--- src/pages/dashboard_atendente.php
+++ src/pages/dashboard_atendente.php
@@ -1,72 +1,106 @@
+<?php
+include('../../config/config.php');
 
+// Número de itens por página
+$items_per_page = 6;
 
-    <div class="container">
-        <div class="row mb-5">
-            <div class="col-1"  >
-                    
-                <div style="background-color: #17a2b8; width: 50px; height: 100%;">
-                    a
+// Página atual
+$current_page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$offset = ($current_page - 1) * $items_per_page;
+
+// Obter o número total de registros
+$sql_total = "SELECT COUNT(*) FROM `medicamentos`";
+$total_result = $conn->query($sql_total);
+$total_rows = $total_result->fetch_row()[0];
+
+// Calcular o número total de páginas
+$total_pages = ceil($total_rows / $items_per_page);
+
+// Consultar registros para a página atual
+$sql = "SELECT * FROM `medicamentos` LIMIT $items_per_page OFFSET $offset";
+$result = $conn->query($sql);
+?>
+
+<div class="container">
+    <div class="row mb-5">
+        <div class="col-1">
+            <div style="background-color: #17a2b8; width: 50px; height: 100%;">a</div>
+        </div>
+        <div class="col-10">
+            <div class="row mt-5">
+                <div class="col mt-3">
+                    <header class="text-white btn-block btn-info" style="padding: 5px; padding-left: 15px;">Entregas:</header>
                 </div>
-                    
             </div>
-            <div class="col-10 ">
-            <div class="row mt-5" >
-                        <div class="col mt-3" >
-                            <header class=" text-white btn-block btn-info" style=" padding: 5px; padding-left: 15px;    ">Entregas:
-                            </header>
-                        </div>
-                    </div>
-
-                 <div class="row">
-                        <div class="col-2 offset-10"><input type="submit" value="Teste" class="btn text-white btn-block     btn-info mt-5">
-                    </div>
+            <div class="row">
+                <div class="col-2 offset-10">
+                    <input type="submit" value="Teste" class="btn text-white btn-block btn-info mt-5">
                 </div>
-                 <div class="row" ><div style="display: flex; justify-content: center;" class="col-10 offset-1">
-
-                         <?php $data = [
-                            ['n' => 1, 'medicamento' => 'Paracetamol', 'tipo' => 'Comprimido', 'categoria' =>   'Analgesico',         'laboratorio' => 'Lab A', 'lote' => 'L123', 'validade' => '2024-12-31',     'quantidade' => 50],
-                            ['n' => 2, 'medicamento' => 'Ibuprofeno', 'tipo' => 'Comprimido', 'categoria' =>    'Antiinflamatório',        'laboratorio' => 'Lab B', 'lote' => 'L456', 'validade' =>   '2025-06-30', 'quantidade' => 100],
-                            // Adicione mais dados conforme necessário
-                        ];
-                        ?>
-
-                    
-                            <table class="table mt-5">
-                                <thead class="thead-light">
-                                    <tr>
-                                        <th scope="col">N°</th>
-                                        <th scope="col">Medicamento</th>
-                                        <th scope="col">Tipo</th>
-                                        <th scope="col">Categoria</th>
-                                        <th scope="col">Laboratório</th>
-                                        <th scope="col">Lote</th>
-                                        <th scope="col">Validade</th>
-                                        <th scope="col">Quantidade</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php foreach ($data as $row): ?>
-                                        <tr>
-                                            <th scope="row"><?php echo $row['n']; ?></th>
-                                            <td><?php echo $row['medicamento']; ?></td>
-                                            <td><?php echo $row['tipo']; ?></td>
-                                            <td><?php echo $row['categoria']; ?></td>
-                                            <td><?php echo $row['laboratorio']; ?></td>
-                                            <td><?php echo $row['lote']; ?></td>
-                                            <td><?php echo $row['validade']; ?></td>
-                                            <td><?php echo $row['quantidade']; ?></td>
-                                        </tr>
-                                    <?php endforeach; ?>
+            </div>
+            <div class="row">
+                <div style="display: flex; justify-content: center;" class="col-10 offset-1">
+                    <table class="table mt-5">
+                        <thead class="thead-light">
+                            <tr>
+                                <th scope="col">N°</th>
+                                <th scope="col">Medicamento</th>
+                                <th scope="col">Tipo</th>
+                                <th scope="col">Categoria</th>
+                                <th scope="col">Laboratório</th>
+                                <th scope="col">Lote</th>
+                                <th scope="col">Validade</th>
+                                <th scope="col">Quantidade</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($result as $row): ?>
+                                <tr>
+                                    <th scope="row"><?= $row['cod_medicamento']; ?></th>
+                                    <td><?= $row['nome_medicamento']; ?></td>
+                                    <td><?= $row['tipo_medicamento']; ?></td>
+                                    <td><?= $row['categoria']; ?></td>
+                                    <td><?= $row['laboratorio']; ?></td>
+                                    <td><?= $row['lote']; ?></td>
+                                    <td><?= $row['validade']; ?></td>
+                                    <td><?= $row['quantidade']; ?></td>
+                                </tr>
+                            <?php endforeach; ?>
                         </tbody>
                     </table>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-12">
+                    <nav aria-label="Page navigation">
+                        <ul class="pagination justify-content-center">
+                            <?php if ($current_page > 1): ?>
+                                <li class="page-item">
+                                    <a class="page-link" href="?page=<?= $current_page - 1; ?>" aria-label="Previous">
+                                        <span aria-hidden="true">&laquo;</span>
+                                    </a>
+                                </li>
+                            <?php endif; ?>
+                            <?php for ($i = 1; $i <= $total_pages; $i++): ?>
+                                <li class="page-item <?= ($i == $current_page) ? 'active' : ''; ?>">
+                                    <a class="page-link" href="?page=<?= $i; ?>"><?= $i; ?></a>
+                                </li>
+                            <?php endfor; ?>
+                            <?php if ($current_page < $total_pages): ?>
+                                <li class="page-item">
+                                    <a class="page-link" href="?page=<?= $current_page + 1; ?>" aria-label="Next">
+                                        <span aria-hidden="true">&raquo;</span>
+                                    </a>
+                                </li>
+                            <?php endif; ?>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </div>
     </div>
+</div>
 
-    <script src="../../assets/js/jquery-3.3.1.min.js"></script>
-    <script src="../../assets/js/popper.min.js"></script>
-    <script src="../../assets/js/bootstrap.min.js"></script>
-    <script src="../../assets/js/main.js"></script>
-    
-    
+<script src="../../assets/js/jquery-3.3.1.min.js"></script>
+<script src="../../assets/js/popper.min.js"></script>
+<script src="../../assets/js/bootstrap.min.js"></script>
+<script src="../../assets/js/main.js"></script>
